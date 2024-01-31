fcb582402aa7faac44775ef6f323133b69424bf9
BukeeRuth <olatunjibukola018@gmail.com>
2024-01-31 12:18:45 -0800

    new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,15 +11,16 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node;
+       
+	new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
 	{
-		/* Allocation failure, return NULL */
 		return (NULL);
 	}
 
-	new_node->value = value;
+	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
